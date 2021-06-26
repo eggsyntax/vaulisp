32441a0cab6193e45e23484a03531b4f59635d6a
Egg Davis <egg.davis@reifyhealth.com>
2021-06-26 13:45:27 -0400

    clojure -X:new :name eggsyntax/vaulisp

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to vaulisp
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/egg/vaulisp_test.clj
@@ -0,0 +1,7 @@
+(ns egg.vaulisp-test
+  (:require [clojure.test :refer :all]
+            [egg.vaulisp :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
